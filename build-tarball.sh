95faa679b8df302264f24bcf674ab70bf9f229ca
Drew Shafer <drew@toopher.com>
2013-10-15 19:26:34 -0500

    Generate html readme

--- build-tarball.sh
+++ build-tarball.sh
@@ -7,6 +7,7 @@ mkdir toopher-openam
 mkdir -p toopher-openam/openam
 cp -r schema toopher-openam
 cp README.md toopher-openam
+python -m markdown README.md > toopher-openam/README.html
 cp -r lib toopher-openam/openam
 cp toopher-openam.jar toopher-openam/openam/lib
 mkdir -p toopher-openam/openam/config
