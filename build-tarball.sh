a2e0a3eba3573aa7adf31b11352df5208e81d29a
Drew Shafer <drew@toopher.com>
2014-01-16 16:08:53 -0600

    Specify python2.7 for markdown->html conversion

--- build-tarball.sh
+++ build-tarball.sh
@@ -12,7 +12,7 @@ mkdir -p toopher-openam/openam/WEB-INF/classes
 
 
 cp README.md toopher-openam
-python -m markdown README.md > toopher-openam/README.html
+python2.7 -m markdown README.md > toopher-openam/README.html
 cp lib/* toopher-openam/openam/WEB-INF/lib/
 cp toopher-openam.jar toopher-openam/openam/WEB-INF/lib/
 rm toopher-openam/openam/WEB-INF/lib/amserver.jar
